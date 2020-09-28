4e6d89070f4dcbb747b0c76e1c7fc011c7b865d5
Armandpl <adpl33@gmail.com>
2020-09-28 18:43:26 +0200

    fix code highlight

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,9 @@ import postcss from "gulp-postcss";
 import atimport from "postcss-import";
 import tailwindcss from "tailwindcss";
 var nested = require("postcss-nested");
+var responsive = require('gulp-responsive');
+const imagemin = require('gulp-imagemin');
+var cachebust = require('gulp-cache-bust');
 
 const SITE_ROOT = "./_site";
 const POST_BUILD_STYLESHEET = `${SITE_ROOT}/assets/css/`;
@@ -41,8 +44,57 @@ task("processStyles", () => {
         ...(isDevelopmentBuild ? [nested()] : [nested(), autoprefixer(), cssnano()]),
       ])
     )
+    .pipe(cachebust({
+        type: 'timestamp'
+    }))
     .pipe(dest(POST_BUILD_STYLESHEET));
 });
+
+task("cacheBust", () => {
+    return src('./_site/*/*.html')
+        .pipe(cachebust({
+            type: 'timestamp'
+        }))
+        .pipe(dest('./_site'));
+})
+
+/*task("processImages", () => {
+    browserSync.notify("Optimizing images...");
+    return src('./assets/images/*')
+        .pipe(
+            responsive({
+                errorOnEnlargement: false,
+                quality: 80,
+                // Convert all images to JPEG format
+                '*': [
+                {
+                    width: 300,
+                    rename: {
+                    extname: '.jpg'
+                    }
+                },
+                {
+                    // Produce 2x images and rename them
+                    width: 300 * 2,
+                    rename: {
+                    suffix: '@2x',
+                    extname: '.jpg'
+                    }
+                },
+                {
+                    // Produce 2x images and rename them
+                    width: 300 * 3,
+                    rename: {
+                    suffix: '@3x',
+                    extname: '.jpg'
+                    }
+                }
+                ]
+            })
+        )
+        .pipe(imagemin())
+        .pipe(dest('./_site/assets/images'))
+});*/
 
 task("startServer", () => {
   browserSync.init({
@@ -72,7 +124,7 @@ task("startServer", () => {
   );
 });
 
-const buildSite = series("buildJekyll", "processStyles");
+const buildSite = series("buildJekyll", "processStyles", "cacheBust");
 
 exports.serve = series(buildSite, "startServer");
 exports.default = series(buildSite);
